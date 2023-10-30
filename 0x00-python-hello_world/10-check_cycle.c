48da19315e729531ae444c0420113c4b211613f0
balmed <iwymed@gamil.com>
2023-10-30 16:19:23 -0700

    file 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * _strncat - Concatenates two strings, using at most n bytes from src.
- * @dest: Destination string.
- * @src: Source string.
- * @n: Maximum number of bytes from src.
+ * check_cycle - check list cycle.
+ * @list: struct of list
  *
  * Return: 1 if cyclical. 0 otherwise.
  */
- int chech_cycle(listint_t *list)
- {
- listint_t *slow = list, *fast = list;
- while (fast && fast->next)
- {
- slow = slow->next;
- fast = fast->next->next;
- if(slow == fast)
- return (1);
- }
- return (0);
- }
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
