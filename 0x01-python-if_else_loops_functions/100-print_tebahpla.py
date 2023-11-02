9f00be362c554b1806c25e0ccc24732c5a165496
balmed <iwymed@gamil.com>
2023-11-01 17:53:11 -0700

    modify 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - 1)), end="")
-    i + 32 if i == 0 else 0
+for i in range(25, -1, -1):
+    c = i + ord('A')
+    if i % 2 == 1:
+        c += 32
+        print("{:c}".format(c), end="")
+    else:
+        print("{:c}".format(c), end="")
 
