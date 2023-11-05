10b17f6586587ef5ac5c3edcb9b2ead05492d942
balmed <iwymed@gamil.com>
2023-11-05 12:33:14 -0800

    modify file 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     list_copy = my_list.copy()
-    if len(my_list) < 1:
+    if len(my_list) == 0:
         return None
     list_copy.sort()
     return list_copy[-1]
