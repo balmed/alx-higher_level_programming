327a1ced2ecf4c69974dfa807b2a0e7578444882
balmed <iwymed@gamil.com>
2023-11-08 17:44:27 -0800

    modif 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        op = sys.argv[2]
-        if op == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif op == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif op == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif op == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        op = {
+                '+': add,
+                '-': sub,
+                '*': mul,
+                '/': div
+                }
+        if argv[2] in op:
+        a = int(argv[1])
+        b = int(argv[3])
+        ops = op[argv[2]]
+        res = ops(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, res))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+        exit(0)
