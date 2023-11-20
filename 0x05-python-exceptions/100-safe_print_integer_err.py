fe92e935da4fdce0fa59eb4a17c4338401ef9427
balmed <iwymed@gamil.com>
2023-11-20 15:50:00 -0800

    modify excetion 1-100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
     else:
