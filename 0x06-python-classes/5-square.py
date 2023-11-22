c9633a33aa6970f283b457ea4666d75e2772d69d
balmed <iwymed@gamil.com>
2023-11-21 17:30:34 -0800

    modify 2 file 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""square module"""
 
 
 class Square:
-    """Represent a square."""
+    """modélise square"""
 
     def __init__(self, size):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
         self.size = size
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -26,11 +21,9 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square with the # character."""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
