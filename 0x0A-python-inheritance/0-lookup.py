9381d800edcaf6380b84ffbb40f7aad34371e3ad
balmed <iwymed@gamil.com>
2023-12-04 15:12:05 -0800

    modify task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-""" define module of class"""
+'''Module the method.'''
+
+
 def lookup(obj):
+    '''Looks up object attributes and methods.
+    Args:
+        obj (object): the object to list.
+
+    Returns:
+        list: the list of attributes.
+    '''
     return dir(obj)
