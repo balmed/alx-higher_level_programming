7324c49573e52a7cda43b409c3232b4bb318c1ef
balmed <iwymed@gamil.com>
2023-12-04 15:17:43 -0800

    modify task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" define module of class dirved or subclass"""
+""" Module of class dirved or subclass"""
 class MyList(list):
+    '''Custom MyList class.'''
     def print_sorted(self):
+        '''Method for printing sorted list.'''
         print(sorted(self))
