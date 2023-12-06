7777ad3698a84fc4bd317752d7cd2464f5f2bfae
balmed <iwymed@gamil.com>
2023-12-05 16:46:39 -0800

    task 8-12

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,6 +3,6 @@
 
 
 def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure 
+    """Return the dictionary represntation of a simple data structure
     (list, dictionary, string, integer & boolean)."""
     return obj.__dict__
