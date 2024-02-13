352b3e9dc5f499dca62a7ce40f75aedbcc6fc9ee
balmed <iwymed@gamil.com>
2024-02-12 16:25:42 -0800

    modify task 7&8

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  const p = Number(process.argv[2]);
+  const  x = Number(process.argv[2]);
   let j = 0;
-  while (j < p) {
+  while (j < x) {
     console.log('C is fun');
    j++;
   }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  const p = Number(process.argv[2]);
+  const x = Number(process.argv[2]);
   let j = 0;
-  while (j < p) {
+  while (j < x) {
     console.log('X'.repeat(x));
     j++;
   }
