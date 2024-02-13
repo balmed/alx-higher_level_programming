247e4d27e421aaf4d8ce7e0946f5ff17f177eb2c
balmed <iwymed@gamil.com>
2024-02-12 16:27:38 -0800

    modify task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  const  x = Number(process.argv[2]);
+  const x = Number(process.argv[2]);
   let j = 0;
   while (j < x) {
     console.log('C is fun');
