a821a69176894736fbbc95ff8d0edeb3e072ff4a
balmed <iwymed@gamil.com>
2024-03-01 14:48:28 -0800

    modify task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,4 @@ import requests
 if __name__ == "__main__":
     req = requests.get('https://alx-intranet.hbtn.io/status')
     content = req.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(content), content))
+    print('Body response:\n\t- type: <class \'{}\'>\n\t- content: {}'.format(type(content).__name__, content))
