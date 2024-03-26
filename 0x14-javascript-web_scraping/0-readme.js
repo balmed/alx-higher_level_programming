38f81fc6dbd4c0c5da12c0118361052b418f687b
balmed <iwymed@gamil.com>
2024-03-26 16:36:03 -0700

    task 0 mdf

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
   if (error) {
-    console.error('Error reading the file:', error);
+	  console.error('Error reading the file:', error);
 
   } else {
-    console.log(content);
+	  console.log(content);
   }
 });
