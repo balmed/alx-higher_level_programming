1b7445a69e78eae587c194ba7396a1a47efb48ba
balmed <iwymed@gamil.com>
2024-03-26 17:03:06 -0700

    task0 mdf

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,15 +4,9 @@ const fs = require('fs');
 // Import Node.js 'fs' module.
 
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  // Use fs.readFile() to read the contents of a file specified argv
-  // 'utf8' specifies the encoding of file being read
-
-  if (error) {
-    // If an error occurs during file read operation, the 'error' parameter will contain an error object.
-    console.error('Error reading the file:', error);
-
-  } else {
-    // If the file is read successfully, the 'content' parameter will contain the contents of file as a string.
-    console.log(content);
-  }
+	if (error) {
+		console.error('Error reading the file:', error);
+	} else {
+		console.log(content);
+	}
 });
